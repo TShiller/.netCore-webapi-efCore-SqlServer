b45bcce0ed1d6d1fe884888a715d8c1f2c257747
ssss <2119622598@qq.com>
2020-06-05 17:50:33 +0800

    实施

--- SHI_INDEX/js/SHI_Login.js
+++ SHI_INDEX/js/SHI_Login.js
@@ -4,20 +4,34 @@ $(document).ready(function(){
 	  //获取登录信息
 	  var email=$('#inputEmail').val();
 	  var pwd=$('#inputPassword').val();
+	 //  $.ajax({
+	 //  	url:'http://localhost:5782/Users/UserLogin',
+	 //  	type:"POST",
+	 //  	async:false,
+		// dataType:"json",
+		// contenttype:"application/json",
+	 //  	data:{"name":email,"pwd":pwd},
+	 //  	success:function(req){
+	 //  		console.info(req);
+	 //  	        //请求成功时处理
+	 //  	},
+	 //  	error:function(){
+	 //  	        //请求出错处理
+	 //  	    }
+	 //  })
+	  var setData={name:email,pwd:pwd};
 	  $.ajax({
-	  	url:'http://localhost:5782/Users/UserLogin',
-	  	type:"POST",
-	  	async:false,
-		dataType:"json",
-		contenttype:"application/json",
-	  	data:{"name":email,"pwd":pwd},
-	  	success:function(req){
-	  		console.info(req);
-	  	        //请求成功时处理
-	  	},
-	  	error:function(){
-	  	        //请求出错处理
-	  	    }
-	  })
+	      type: 'POST',
+	      url: 'http://localhost:5782/Users/UserLogin',
+	      dataType: 'json',
+	      contentType: "application/json",
+	      data: JSON.stringify(setData),
+	      success: function(data){
+	          alert('数据加载成功');
+	      },
+	      error: function(xhr, type){
+	          alert('数据加载失败');
+	      }
+	  });
 	});
 }); 
