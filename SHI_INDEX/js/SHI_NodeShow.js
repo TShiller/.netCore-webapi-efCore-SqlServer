882dee699d71c8882d5b2a46521209ca28e83aea
ssss <2119622598@qq.com>
2020-06-17 10:39:01 +0800

    更新

--- SHI_INDEX/js/SHI_NodeShow.js
+++ SHI_INDEX/js/SHI_NodeShow.js
@@ -13,3 +13,41 @@ $(function(){
 	$('#erji').width($('#geren').width());
 	$('#erji li').width($('#geren').width());
 })
+
+//收缩左导航
+$('#shousuo').click(function(){
+	$('#shou_in').toggle();
+	$('#shou_up').toggle();
+	if($('#zuodaohang').width()<50){
+		$('#zuodaohang').width('17%');
+		$('#shousuo').width('17%');
+	}
+	else{
+		$('#zuodaohang').width(40);
+		$('#shousuo').width(40);
+	}
+})
+
+layui.use('tree', function(){
+    var tree = layui.tree;
+   
+    //渲染
+    var inst1 = tree.render({
+      elem: '#test1'  //绑定元素
+      ,data: [{
+        title: '江西' //一级菜单
+        ,children: [{
+          title: '南昌' //二级菜单
+          ,children: [{
+            title: '高新区' //三级菜单
+            //…… //以此类推，可无限层级
+          }]
+        }]
+      },{
+        title: '陕西' //一级菜单
+        ,children: [{
+          title: '西安' //二级菜单
+        }]
+      }]
+    });
+  });
