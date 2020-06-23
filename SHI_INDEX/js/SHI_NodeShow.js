0a7a0dba12f773c16d701f3f8f0a276fae15ad7d
ssss <2119622598@qq.com>
2020-06-23 11:48:05 +0800

    完善

--- SHI_INDEX/js/SHI_NodeShow.js
+++ SHI_INDEX/js/SHI_NodeShow.js
@@ -3,7 +3,7 @@ $(function(){
     var cookies=$.cookie('shi_token');
 	if(cookies==null){
 		//window.history.back(-1);//返回上一页
-		//window.location.href='SHI_Login.html';
+		window.location.href='SHI_Login.html';
 	}
 	var screenHeight=$(window).height();//获取屏幕高度
 	var heights=screenHeight-56;//减去导航的高度
